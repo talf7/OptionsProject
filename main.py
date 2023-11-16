d525d2b07fc76154f355ad46075e64a39240bf9b
talf7 <talfischer7.tf@gmail.com>
2023-11-16 21:25:27 +0200

    fixed issue and added initialize function

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os.path
 
 def PickActivity(options,formerActivity):
     while True:
+        print(options)
         currentChoice = random.choice(list(options.keys()))
         options[currentChoice] += 1
         if options[currentChoice] == 3:
@@ -25,7 +26,7 @@ def PickActivity(options,formerActivity):
     return currentChoice,options
 def EditChoice(options, y0rn):
     while y0rn == 'y':
-        ans = input("would you like to add ,remove or edit an option?\nEnter '1' to add an option\nEnter '2' to remove an option\nEnter '3' to replace an option\nEnter 'n' for no\n")
+        ans = input("would you like to add ,remove or replace an option?\nEnter '1' to add an option\nEnter '2' to remove an option\nEnter '3' to replace an option\nEnter 'n' for no\n")
         if ans == '1':
             options = AddChoice(options)
         elif ans == '2':
@@ -34,32 +35,27 @@ def EditChoice(options, y0rn):
             replaceChoice(options,'y')
         else:
             break
+    return options
 #    yOrn = input("would you like to add ,remove or edit another option? 'y' for yes, 'n' for no \n")
 def replaceChoice(options, unwantedChoice):
-    ans = 'y'
-    while ans == 'y':
-        ShowActivities(options)
-        unwantedChoice = input("enter the option you would like to replace:\n")
-        if unwantedChoice not in options:
-            print("the option is not in the list so it cant be replaced")
+    ShowActivities(options)
+    unwantedChoice = input("enter the option you would like to replace:\n")
+    if unwantedChoice not in options:
+        print("the option is not in the list so it cant be replaced")
+    else:
+        wantedChoice = input("Enter the desired option:\n")
+        if wantedChoice in options:
+            print("the option is already in the list")
         else:
-            wantedChoice = input("Enter the desired option:\n")
-            if wantedChoice in options:
-                print("the option is already in the list")
-            else:
-                options[wantedChoice] = 0
-                del options[unwantedChoice]
-        ans = input("would you like to edit another option? 'y' for yes, 'n' for no \n")
+            options[wantedChoice] = 0
+            del options[unwantedChoice]
     return options
 def AddChoice(options):
-    yOrn = 'y'
-    while yOrn == "y":
-        option = input("Enter option:\n")
-        if option not in options:
-            options[option] = 0
-            yOrn = input("would you like to put another option?\n'y' for yes\n'n' for no\n")
-        else:
-            print("The option is already in the pool\n")
+    option = input("Enter option:\n")
+    if option not in options:
+        options[option] = 0
+    else:
+        print("The option is already in the pool\n")
     return options
 def RemoveChoice(options):
         ShowActivities(options)
@@ -78,9 +74,18 @@ def ShowActivities(options):
 
 
 formerActivity = ""
+
+
+def Initialize(options):
+    firstOption = input("Enter your first option to add\n")
+    options[firstOption] = 0
+    return EditChoice(options,'y')
+
+
+
 if not os.path.isfile("options.txt"):
     options = {}
-    options = AddChoice(options)
+    options = Initialize(options)
 else:
     fileRead = open("options.txt","r")
     options = eval(fileRead.readline())
@@ -92,17 +97,9 @@ else:
     elif answer == 'n':
         PickActivity(options, formerActivity)
 
-
-
 choice , options = PickActivity(options,formerActivity)
 f = open("options.txt","w")
 f.write(str(options) + "\nformerActivity=" + choice)
 
 f.close()
-
-#{"coding":0,"STS":0,"rubik":0,"otherPS":0,"HellCard":0}
-
-
-
-# best of 3:
 
