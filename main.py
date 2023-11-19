a9f503fab2bcb82259ef005ccaa2dfe1e85289cb
shilo270 <shinigami270@gmail.com>
2023-11-19 17:42:33 +0200

    Main menu + Statistics

--- main.py
+++ main.py
@@ -2,22 +2,8 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import os.path
 import Logic as L
+L.UpdateFile()
 
-if not os.path.isfile("options.txt"):
-    options = {}
-    formerActivity = ""
-    options = L.Initialize(options)
-else:
-    fileRead = open("options.txt","r")
-    options = eval(fileRead.readline())
-    formerActivity = fileRead.readline().split("=")[1]
-    L.ShowActivities(options)
-    options = L.EditChoice(options)
 
-choice, options = L.PickActivity(options,formerActivity)
-f = open("options.txt","w")
-f.write(str(options) + "\nformerActivity=" + choice)
-f.close()
 
